f172492e4c9fb94da12570186393713cb60df0d5
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2021-06-10 22:42:57 +0000

    add deprecation warning

--- kerastuner/__init__.py
+++ kerastuner/__init__.py
@@ -1,1 +1,8 @@
+import warnings
 from keras_tuner import *
+
+warnings.warn(
+    "`import kerastuner` is deprecated, please use `import keras_tuner`.",
+    DeprecationWarning,
+    stacklevel=2,
+)
