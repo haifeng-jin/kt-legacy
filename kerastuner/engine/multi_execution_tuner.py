12733635169d68827fafbb781c9e324424867660
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-04-12 15:57:20 -0700

    Update multi_execution_tuner.py

--- kerastuner/engine/multi_execution_tuner.py
+++ kerastuner/engine/multi_execution_tuner.py
@@ -1,1 +1,4 @@
-from keras_tuner.engine.multi_execution_tuner import *
+try:
+    from keras_tuner.engine.multi_execution_tuner import *
+except ImportError:
+    from keras_tuner.engine.tuner import Tuner as MultiExecutionTuner
